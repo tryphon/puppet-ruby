d0c07d21a4f97fd5457b8e50d70903e0b47275a3
Alban Peignier <alban.peignier@free.fr>
2010-09-17 12:21:29 +0200

    Create a /usr/local/bin/bundle link to include bundle in default PATH

--- manifests/bundler.pp
+++ manifests/bundler.pp
@@ -1,4 +1,9 @@
 class ruby::bundler {
   include ruby::gems
   ruby::gem { bundler: ensure => latest }
+
+  file { "/usr/local/bin/bundle":
+    ensure => "/var/lib/gems/1.8/bin/bundle",
+    require => Ruby::Gem[bundler]
+  }
 }
