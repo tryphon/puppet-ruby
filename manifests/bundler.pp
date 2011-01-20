ec63cda1a5fd25712027040a7d65b592a441189c
Alban Peignier <alban@tryphon.eu>
2011-01-20 14:56:52 +0100

    Temporary bundler version restriction (workaround for Rails 2.3.x troubles)

--- manifests/bundler.pp
+++ manifests/bundler.pp
@@ -1,6 +1,8 @@
 class ruby::bundler {
   include ruby::gems
-  ruby::gem { bundler: ensure => latest }
+
+  # FIXME: troubles with Rails 2.3.8 and bundler >= 1.0.8
+  ruby::gem { bundler: ensure => "1.0.7" }
 
   file { "/usr/local/bin/bundle":
     ensure => "/var/lib/gems/1.8/bin/bundle",
