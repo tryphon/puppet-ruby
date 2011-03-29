18c9a7f655b333a3e3b53abc31d31a0f690eb371
Alban Peignier <alban@tryphon.eu>
2011-03-29 17:11:13 +0200

    Use hoe 2.8.0 (to avoid problem with rubygems version)

--- manifests/syslog.pp
+++ manifests/syslog.pp
@@ -1,3 +1,5 @@
 class ruby::syslog {
-  ruby::gem { SyslogLogger: }
+  ruby::gem { SyslogLogger: require => Ruby::Gem[hoe] }
+  # hoe > 2.9 requires rubygems 1.4
+  ruby::gem { hoe: ensure => '2.8.0' }
 }
