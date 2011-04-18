998d733300eb17f27f43a21618379f1ce118aae3
Alban Peignier <alban@tryphon.eu>
2011-04-18 16:13:49 +0200

    Create ruby::gem::sqlite3::dependencies

--- manifests/gems.pp
+++ manifests/gems.pp
@@ -30,3 +30,22 @@ class ruby::gem::nokogiri::dependencies {
   package { [libxml2-dev, zlib1g-dev]: }  
   package { libxslt1-dev: }  
 }
+
+class ruby::gem::sqlite3::dependencies {
+  include ruby::gems
+
+  package { libsqlite3-dev:
+    require => Apt::Preferences[libsqlite3-dev]
+  }
+  apt::preferences { libsqlite3-dev:
+    package => libsqlite3-dev,
+    pin => "release a=lenny-backports",
+    priority => 999,
+    require => Apt::Preferences[libsqlite3]
+  }
+  apt::preferences { libsqlite3:
+    package => libsqlite3-0,
+    pin => "release a=lenny-backports",
+    priority => 999
+  }
+}
