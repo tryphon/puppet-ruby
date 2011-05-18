9e562d0c0f3f80ce6c63b4d3fe2db3093a9e4aff
Alban Peignier <alban@tryphon.eu>
2011-05-18 10:54:42 +0200

    Don't use rubygems from backports for squeeze

--- manifests/gems.pp
+++ manifests/gems.pp
@@ -1,21 +1,25 @@
 class ruby::gems {
   include ruby::dev
-  include apt::tryphon
 
   package { rubygems: 
-    ensure => latest,
-    require => [Apt::Sources_list[tryphon], Apt::Preferences[rubygems]]
+    ensure => latest
   }
 
-  apt::preferences { rubygems:
-    package => rubygems, 
-    pin => "release a=lenny-backports",
-    priority => 999
-  }
-  apt::preferences { "rubygems1.8":
-    package => "rubygems1.8", 
-    pin => "release a=lenny-backports",
-    priority => 999
+  if ($lsbdistcodename != 'squeeze') {
+    include apt::tryphon
+    Package[rubygems] {
+      require => [Apt::Sources_list[tryphon], Apt::Preferences[rubygems]]
+    }
+    apt::preferences { rubygems:
+      package => rubygems, 
+      pin => "release a=lenny-backports",
+      priority => 999
+    }
+    apt::preferences { "rubygems18":
+      package => "rubygems1.8", 
+      pin => "release a=lenny-backports",
+      priority => 999
+    }
   }
 
   file { "/etc/gemrc":
