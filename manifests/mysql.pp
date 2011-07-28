6459c8c556983bda23673ea23ec6e4ea712f46c5
Alban Peignier <alban@tryphon.eu>
2011-07-28 19:09:38 +0200

    Keep using libmysqlclient15-dev on lenny

--- manifests/mysql.pp
+++ manifests/mysql.pp
@@ -1,7 +1,12 @@
 class ruby::mysql {
   include ruby::gems
   include mysql::client  
-  package { libmysqlclient-dev: }
+
+  if $debian::lenny {
+    package { libmysqlclient15-dev: alias => libmysqlclient-dev }
+  } else {
+    package { libmysqlclient-dev: }
+  }
 
   ruby::gem { mysql: require => Package[libmysqlclient-dev] }
 }
