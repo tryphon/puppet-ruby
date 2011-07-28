6699953073f55d29f98fac7221e798d331c481da
Alban Peignier <alban@tryphon.eu>
2011-07-28 17:15:16 +0200

    Use libmysqlclient-dev for mysql gem

--- manifests/mysql.pp
+++ manifests/mysql.pp
@@ -1,7 +1,7 @@
 class ruby::mysql {
   include ruby::gems
   include mysql::client  
-  package { libmysqlclient15-dev: }
+  package { libmysqlclient-dev: }
 
-  ruby::gem { mysql: require => Package[libmysqlclient15-dev] }
+  ruby::gem { mysql: require => Package[libmysqlclient-dev] }
 }
