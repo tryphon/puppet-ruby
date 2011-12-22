b2f86ab09bc8466de1482b8f06ad5bf66d9cd4de
Alban Peignier <alban@tryphon.eu>
2011-12-22 14:06:30 +0100

    Add cap-fix-permissions tool to fix permissions in files deployed by capistrano

--- manifests/capistrano.pp
+++ manifests/capistrano.pp
@@ -1,3 +1,12 @@
 class ruby::capistrano {
   ruby::gem { [capistrano, capistrano-ext]: }
+
+  file { "/usr/local/sbin/cap-fix-permissions":
+    source => "puppet:///ruby/cap-fix-permissions",
+    mode => 755
+  }
+
+  sudo::user_line { "cap-fix-permissions":
+    line => "%src	ALL=NOPASSWD: /usr/local/sbin/cap-fix-permissions"
+  }
 }
