29c8904ab1b417431eb02dffd1800a895f2d2d2d
Alban Peignier <alban@tryphon.eu>
2012-01-03 13:08:33 +0100

    Move ruby::capistrano::target to a dedicated file

---
+++ manifests/capistrano/target.pp
@@ -0,0 +1,13 @@
+# Tools used remotely by capistrano scripts
+class ruby::capistrano::target {
+  include git::common
+
+  file { "/usr/local/sbin/cap-fix-permissions":
+    source => "puppet:///ruby/cap-fix-permissions",
+    mode => 755
+  }
+
+  sudo::user_line { "cap-fix-permissions":
+    line => "%src	ALL=NOPASSWD: /usr/local/sbin/cap-fix-permissions"
+  }
+}
