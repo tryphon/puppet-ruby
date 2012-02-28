f6caec41dab44ba066b866e33653cfc551b64214
Alban Peignier <alban@tryphon.eu>
2012-02-28 11:16:46 +0100

    No longer creates /usr/local/bin/bundle link

--- manifests/bundler.pp
+++ manifests/bundler.pp
@@ -3,8 +3,8 @@ class ruby::bundler {
 
   ruby::gem { bundler: }
 
-  file { "/usr/local/bin/bundle":
-    ensure => "/var/lib/gems/1.8/bin/bundle",
-    require => Ruby::Gem[bundler]
-  }
+  # file { "/usr/local/bin/bundle":
+  #   ensure => "/var/lib/gems/1.8/bin/bundle",
+  #   require => Ruby::Gem[bundler]
+  # }
 }
