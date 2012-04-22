788b4cbfb55b887bc4f0526c6e1a29c7d78243b1
Alban Peignier <alban@tryphon.eu>
2012-04-22 14:10:11 +0200

    Don't creat /usr/local/bin/rake link

--- manifests/rake.pp
+++ manifests/rake.pp
@@ -1,7 +1,8 @@
 class ruby::rake {
   # rake package is too old into lenny
   ruby::gem { rake: }
-  file { "/usr/local/bin/rake":
-    ensure => "/var/lib/gems/1.8/bin/rake"
-  }
+
+  # file { "/usr/local/bin/rake":
+  #   ensure => "/var/lib/gems/1.8/bin/rake"
+  # }
 }
