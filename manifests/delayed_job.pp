8ccd67878bfc9944a78de195847e4e495bc0445a
Alban Peignier <alban@tryphon.eu>
2012-07-25 10:22:12 +0200

    Add ruby::delayed_job::initd define to start/restart delayed_job

---
+++ manifests/delayed_job.pp
@@ -0,0 +1,12 @@
+define ruby::delayed_job::initd {
+  file { "/etc/init.d/$name":
+    source => "puppet:///ruby/delayed_job.init",
+    mode => 755
+  }
+
+  exec { "insserv-$name":
+    command => "insserv $name",
+    require => File["/etc/init.d/$name"],
+    unless => "ls /etc/rc?.d/S*$name > /dev/null 2>&1"
+  }
+}
