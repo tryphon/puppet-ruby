413d28d107f51eb693b594c3ffb23bc23d02d6a3
Alban Peignier <alban@tryphon.eu>
2013-06-28 17:31:47 +0200

    Add ruby::rake::application define

--- manifests/rake.pp
+++ manifests/rake.pp
@@ -5,4 +5,35 @@ class ruby::rake {
   # file { "/usr/local/bin/rake":
   #   ensure => "/var/lib/gems/1.8/bin/rake"
   # }
+
+  define application($server_name = false, $environment = "production") {
+    if $server_name {
+      $site_name = regsubst($server_name, '\.', '_', 'G')
+      apache2::site { $site_name:
+        content => template("ruby/rails/apache.conf")
+      }
+    }
+
+    file { ["/var/www/$name", "/var/www/$name/shared"]:
+      ensure => directory,
+      mode => 2775,
+      group => src
+    }
+    file { "/var/www/$name/shared/config":
+      ensure => "/etc/$name",
+      force => true
+    }
+
+    if ! defined(File["/etc/$name"]) {
+      file { "/etc/$name":
+        ensure => directory
+      }
+    }
+
+    exec { "restart-$name":
+      refreshonly => true,
+      command => "touch /var/www/$name/current/tmp/restart.txt",
+      onlyif => "test -d /var/www/$name/current/tmp/"
+    }
+  }
 }
