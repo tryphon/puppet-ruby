0e4e422a2d96dbfa661872f648132aa722738c4e
Alban Peignier <alban@tryphon.eu>
2014-02-21 19:32:59 +0100

    Update operating_system.rb with latest version

--- files/operating_system.rb
+++ files/operating_system.rb
@@ -12,7 +12,18 @@ class << Gem
 
   alias :upstream_default_path :default_path
   def default_path
-    upstream_default_path + [File.join('/usr/share/rubygems-integration', Gem::ConfigMap[:ruby_version])]
+    upstream_default_path + [File.join('/usr/share/rubygems-integration', Gem::ConfigMap[:ruby_version]), '/usr/share/rubygems-integration/all']
   end
 
+end
+
+if RUBY_VERSION >= '2.0' then
+  class << Gem::Specification
+
+    alias :upstream_default_specifications_dir :default_specifications_dir
+    def default_specifications_dir
+      File.join(Gem.upstream_default_dir, "specifications", "default")
+    end
+
+  end
 end
