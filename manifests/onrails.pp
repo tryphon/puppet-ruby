34b3ea33cb29d3800b6b48e638c104dde315d486
Alban Peignier <alban@tryphon.eu>
2015-09-28 18:36:05 +0200

    Remove useless default gems (now in bundler)

--- manifests/onrails.pp
+++ manifests/onrails.pp
@@ -1,9 +1,9 @@
 class ruby::onrails {
   include apache2::passenger
-  include ruby::gems
-  include ruby::rake
-  include ruby::irb
-  include ruby::syslog
+  # include ruby::gems
+  # include ruby::rake
+  # include ruby::irb
+  # include ruby::syslog
   include ruby::capistrano::target
 
   file { "/usr/local/bin/rails-console":
