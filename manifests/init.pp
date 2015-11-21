73581afe807c6b8bb8f1af8363b0eec22ccf48c2
Alban Peignier <alban@tryphon.eu>
2015-11-21 15:14:01 +0100

    Fixes (try to -) rake default installation with ruby 2.2

--- manifests/init.pp
+++ manifests/init.pp
@@ -61,6 +61,13 @@ class ruby22 {
     ensure => directory,
     before => File["/usr/lib/ruby/vendor_ruby/rubygems/defaults/operating_system.rb"]
   }
+
+  # rake gem is present in bearstech package but rake script isn't present into /var/lib/gems/2.2.0/bin/rake
+  exec { 'ruby-gem22-fix-rake':
+    command => 'gem2.2 install rake',
+    creates => '/var/lib/gems/2.2.0/bin/rake',
+    require => File['/usr/lib/ruby/vendor_ruby/rubygems/defaults/operating_system.rb']
+  }
 }
 
 class ruby::multiversion::2 {
